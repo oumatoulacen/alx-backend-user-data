d620e64998444cee534a35c63e5d18e67411696f
oumatoulacen <oumatoulacen@gmail.com>
2024-02-08 23:19:11 +0100

    files + log message obfuscated with regex

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+'''Write a function called filter_datum that returns the log message obfuscated:
+'''
+import re
+from typing import List
+import logging
+
+
+def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
+    '''returns the log message obfuscated'''
+    for field in fields:
+        message = re.sub(rf'{field}=.+?{separator}',
+                         f'{field}={redaction}{separator}', message)
+    return message
