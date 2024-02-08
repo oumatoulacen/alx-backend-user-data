879d9397da99c81aac397e11338aaebe413b8816
oumatoulacen <oumatoulacen@gmail.com>
2024-02-09 00:56:07 +0100

    Log formatter..

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -18,20 +18,31 @@ def filter_datum(fields: List[str],
 
 
 class RedactingFormatter(logging.Formatter):
-    """ Redacting Formatter class
-        """
+    """Redacting Formatter class that
+    redacts sensitive data in log messages."""
 
     REDACTION = "***"
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
-    def __init__(self, fields: list[str]):
+    def __init__(self, fields: List[str]):
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
-        '''returns the log message obfuscated'''
+        """
+        Format the log record and redact sensitive data.
+
+        Args:
+            record (logging.LogRecord): LogRecord
+            instance containing the log message.
+
+        Returns:
+            str: The formatted log message with sensitive data redacted.
+        """
         message = super(RedactingFormatter, self).format(record)
-        logs = filter_datum(self.fields, self.REDACTION,
-                            message, self.SEPARATOR)
-        return logs
+        redacted = filter_datum(self.fields, self.REDACTION,
+                                message, self.SEPARATOR)
+        return redacted
+
+
