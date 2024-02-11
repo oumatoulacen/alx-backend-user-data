ecbe2ea0062a5fc4ddb1efe18357ed7dc0dcc59b
oumatoulacen <oumatoulacen@gmail.com>
2024-02-11 04:19:45 +0100

    encrypt password with bcrypt

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -1,1 +1,8 @@
 #!/usr/bin/env python3
+'''Encrypting passwords'''
+import bcrypt
+
+
+def hash_password(password: str) -> str:
+    '''returns a hashed password'''
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
