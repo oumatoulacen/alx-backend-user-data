6ef24be7bf8f125cc87a4195b0dfa07f92a1f8cb
oumatoulacen <oumatoulacen@gmail.com>
2024-02-13 17:43:47 +0100

    Request validation!

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -28,9 +28,9 @@ def before_request():
                              ['/api/v1/status/', '/api/v1/unauthorized/',
                               '/api/v1/forbidden/']):
             if auth.authorization_header(request) is None:
-                    abort(401)
+                abort(401)
             if auth.current_user(request) is None:
-                    abort(403)
+                abort(403)
 
 
 @app.errorhandler(404)
