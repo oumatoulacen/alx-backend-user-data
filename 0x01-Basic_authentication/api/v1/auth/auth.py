a131336a1aa8faa07b64b8962bd8eef8ce109b0e
oumatoulacen <oumatoulacen@gmail.com>
2024-02-13 17:42:12 +0100

    Request validation!

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -22,7 +22,11 @@ class Auth:
     def authorization_header(self, request=None) -> str:
         """ authorization_header method
         """
-        return None
+        if request is None:
+            return None
+        if request.headers.get('Authorization') is None:
+            return None
+        return request.headers.get('Authorization')
 
     def current_user(self, request=None) -> TypeVar('User'):  # type: ignore
         """ current_user method
