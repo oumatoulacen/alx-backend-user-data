c9caefd7be14b010002f19f1af322651456fb1c8
oumatoulacen <oumatoulacen@gmail.com>
2024-02-14 01:19:16 +0100

    Basic - User object

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 ''' Module of Auth views'''
-from typing import List, TypeVar
+from typing import List, Tuple, TypeVar
 from flask import request, jsonify, abort
 import base64
 from api.v1.auth.auth import Auth
+from models.user import User
 
 
 class BasicAuth(Auth):
@@ -31,5 +32,35 @@ class BasicAuth(Auth):
         try:
             return base64.b64decode(
                 base64_authorization_header).decode('utf-8')
+        except Exception:
+            return None
+
+    def extract_user_credentials(self,
+                                 decoded_base64_authorization_header: str
+                                 ) -> Tuple[str, str]:
+        '''return the user email and password from the Base64 decoded value'''
+        if decoded_base64_authorization_header is None:
+            return None, None
+        if not isinstance(decoded_base64_authorization_header, str):
+            return None, None
+        if ':' not in decoded_base64_authorization_header:
+            return None, None
+        return tuple(decoded_base64_authorization_header.split(':', 1))
+
+    def user_object_from_credentials(self, user_email: str,
+                                     user_pwd: str) -> TypeVar('User'):
+        '''return the User instance based on his email and password'''
+        if user_email is None or not user_email:
+            return None
+        if user_pwd is None or not user_pwd:
+            return None
+        try:
+            user = User.search({'email': user_email})[0]
+            if user is None:
+                return None
+            if user.is_valid_password(user_pwd):
+                return user
+            else:
+                return None
         except Exception:
             return None
