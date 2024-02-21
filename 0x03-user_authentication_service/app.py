d82ea0f9bd80d3416a49c534175f7b8c05018cd3
oumatoulacen <oumatoulacen@gmail.com>
2024-02-21 02:57:44 +0100

    Basic Flask app/Register user

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -34,7 +34,7 @@ def users() -> str:
         AUTH.register_user(email, password)
         return jsonify({"email": email, "message": "user created"})
     except ValueError:
-        return jsonify({"message": "email already registered"})
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
