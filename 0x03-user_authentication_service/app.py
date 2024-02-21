0bf989d73d93c1d56ff3945e21fbd40c32137839
oumatoulacen <oumatoulacen@gmail.com>
2024-02-21 05:30:52 +0100

    User profile

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -83,5 +83,21 @@ def delete_session() -> str:
     abort(403)
 
 
+@app.route('/profile', methods=['GET'], strict_slashes=False)
+def profile() -> str:
+    """GET /profile
+    JSON body:
+        - session_id
+    Return:
+        - {"email": "<registered email>"} if success
+        - abort with a 403 if error
+    """
+    session_id = request.cookies.get('session_id')
+    user = AUTH.get_user_from_session_id(session_id)
+    if user:
+        return jsonify({"email": user.email})
+    abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
