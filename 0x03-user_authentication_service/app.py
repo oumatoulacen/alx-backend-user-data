93b08829834375c8f0bb8fe68db0eb6f54517219
oumatoulacen <oumatoulacen@gmail.com>
2024-02-21 02:05:17 +0100

    Basic Flask app

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,1 +1,21 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""App module
+"""
+from flask import Flask, jsonify, request, abort, make_response
+from auth import Auth
+
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'], strict_slashes=False)
+def welcome() -> str:
+    """GET /
+    Return:
+      - welcome message
+    """
+    return jsonify({"message": "Bienvenue"})
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
