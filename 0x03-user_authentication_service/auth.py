c10cc1fdf2deab8af080229763f5662eef43d94b
oumatoulacen <oumatoulacen@gmail.com>
2024-02-21 01:17:00 +0100

    Hash a password

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,1 +1,8 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+'''auth module.'''
+import bcrypt
+
+
+def _hash_password(password: str) -> bytes:
+    '''Hash a password.'''
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
