1e8c39415f891881925d12cbf682fdf24dda6790
oumatoulacen <oumatoulacen@gmail.com>
2024-02-21 03:16:47 +0100

    Generate UUIDs/ rearrange

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -12,6 +12,11 @@ def _hash_password(password: str) -> bytes:
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
 
+def _generate_uuid(self) -> str:
+    '''Generate a UUID.'''
+    return str(uuid.uuid4())
+
+
 class Auth:
     """Auth class to interact with the authentication database.
     """
@@ -43,7 +48,3 @@ class Auth:
                                   user.hashed_password)
         except NoResultFound:
             return False
-
-    def _generate_uuid(self) -> str:
-        '''Generate a UUID.'''
-        return str(uuid.uuid4())
