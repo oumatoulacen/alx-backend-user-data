c763f28cb3d1a5e02e4a671fd954e7ed0bd02f2b
oumatoulacen <oumatoulacen@gmail.com>
2024-02-21 03:08:24 +0100

    Credentials validation

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -38,7 +38,7 @@ class Auth:
             return False
         try:
             user = self._db.find_user_by(email=email)
-            return bcrypt.checkpw(password.encode('utf-8'), user.hashed_password)
+            return bcrypt.checkpw(password.encode('utf-8'),
+                                  user.hashed_password)
         except NoResultFound:
             return False
-    
